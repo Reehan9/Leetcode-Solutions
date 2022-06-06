bcc1e28de80d54d54533825e865541b173be8946
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-06-06 19:33:33 +0530

    Add files via upload

---
+++ 1. Two Sum/1. Two Sum.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int ,int>check;
+        for(int i = 0  ; i < nums.size() ; i++){
+            if(check.find(target-nums[i])!=check.end()){
+                return {i , check[target-nums[i]] };
+            }
+              check[nums[i]] = i ;
+        }
+          return{};
+    }
+};
