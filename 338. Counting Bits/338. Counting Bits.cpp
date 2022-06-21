c1bdca4a704dd21ccb12a3f7683e7b20da0b1bb8
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-06-21 12:30:32 +0530

    Update 338. Counting Bits.cpp

--- 338. Counting Bits/338. Counting Bits.cpp
+++ 338. Counting Bits/338. Counting Bits.cpp
@@ -9,3 +9,16 @@ public:
             return answer; // returning the vector
     }
 };
+/*
+Alternate solution to save space time complexity with few edits (Best Solution)
+class Solution {
+public:
+    vector<int> countBits(int n) {
+            vector<int>answer(n+1);
+        for(int i = 0 ; i <= n ; i++){
+                bitset<32>p = i;
+                answer[i] = p.count();
+        }
+            return answer;
+    }
+};*/
