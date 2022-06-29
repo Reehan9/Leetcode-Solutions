c1161759dc015bb9b9e0454da94493d0c626cc14
Syed Reehan <92086803+Reehan9@users.noreply.github.com>
2022-06-29 10:26:33 +0530

    Add files via upload

---
+++ 83. Remove Duplicates from Sorted List/83.Remove Duplicates from Sorted List.cpp
@@ -0,0 +1,27 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode() : val(0), next(nullptr) {}
+ *     ListNode(int x) : val(x), next(nullptr) {}
+ *     ListNode(int x, ListNode *next) : val(x), next(next) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* deleteDuplicates(ListNode* head) {
+        unordered_map<int , int>check;
+        ListNode* h1 = head;
+        ListNode* h2 = NULL;
+        while(h1!=NULL){
+           check[h1->val]++;
+            if(check[h1->val]>1){
+                h2->next = h1->next;
+            }
+            h2 = h1;
+            h1 = h1->next;
+        }
+        return head;
+    }
+};
