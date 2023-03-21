8928804a1523761f4f981b42614a8c5c653cc3b4
Reehan <92086803+Reehan9@users.noreply.github.com>
2023-03-22 00:42:26 +0530

    Create main.cpp

---
+++ 00101. Symmetric Tree/main.cpp
@@ -0,0 +1,11 @@
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        return check(root->left , root->right);
+    }
+   bool check(TreeNode*left , TreeNode*right){
+       if(left == NULL && right == NULL ) return true;
+       if(left == NULL || right == NULL || left->val != right->val) return false;
+       return  check(left->left , right->right)&& check(left->right , right->left);
+   }
+};
